049a8e642a7499f03a285cd19d6bfb46b0f2cdb1
Ashish jha <ajha5645@gmail.com>
2026-01-30 19:10:28 +0530

    Add basename to BrowserRouter for GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import ProtectedRoute from './components/ProtectedRoute.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/web-tools">
       <AuthProvider>
         <Toaster
           position="top-center"
